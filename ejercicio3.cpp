4837de32eb4433a37699ae2e98bbfcd72de06827
juanplayer0 <45865900+juanplayer0@users.noreply.github.com>
2026-02-19 20:54:53 -0500

    ejercicio sumar los elementos de un arreglo

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int n;
+    cout << "ingrese la cantidad de elementos: ";
+    cin >> n;
+    
+    int arreglo[n];
+    int suma = 0;
+
+    for (int i=0; i <n; i++){
+        cout << "ingrese el elemento" << i + 1 << ": ";
+        cin >> arreglo[i];
+    }
+    
+    for (int i = 0; i < n; i++){
+        suma += arreglo[i];
+    }
+    cout << "la suma de los elementos es: " << suma << endl;
+
+    return 0;
+}
