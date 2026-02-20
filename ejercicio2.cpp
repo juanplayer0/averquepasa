623556daaeed26e1ace74492a25a1d9b421ef84c
juanplayer0 <45865900+juanplayer0@users.noreply.github.com>
2026-02-19 20:43:02 -0500

    ejercicio calculo interes compuesto

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+#include <cmath>
+int main(){
+    double capital, tasa, monto;
+    int tiempo;
+    cout << "ingrese el capital inicial aqui";
+    cin >> capital;
+    cout << "ingrese la tasa de interes:";
+    cin >> tasa;
+    cout <<"ingrese el numero de periodos: ";
+    cin >> tiempo;
+    monto = capital * pow((1+tasa), tiempo);
+    cout << "el monto final es:" <<monto <<endl;
+    return 0;
+}
