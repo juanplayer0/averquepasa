9f64e72184e31fddef4f7a97cc373a3220864076
juanplayer0 <45865900+juanplayer0@users.noreply.github.com>
2026-02-19 21:25:50 -0500

    ejercicio palindromo

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+bool espalindromo(int x){
+    if (x < 0 || (x % 10 == 0 && x != 0))
+    return false;
+
+    int medioreverso = 0;
+    while (x > medioreverso){
+        medioreverso = medioreverso * 10 + x % 10;
+        x /= 10;
+    }
+
+    return (x == medioreverso || x == medioreverso / 10);
+
+}
+int main(){
+    
+    int x;
+    cout << "ingrese un numero: ";
+    cin >> x;
+
+    if (espalindromo(x))
+        cout << "true" << endl;
+    else
+        cout << "false" << endl;
+    return 0;
+}
